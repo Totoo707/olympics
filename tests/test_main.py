07c3137e4ac34bd51126e93be1912721b5852ffb
Thomas Lozancic <tlozancic69@gmail.com>
2024-10-04 18:28:39 +0200

    api 100%

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,3 +4,12 @@ from olympics.__main__ import main
 def test_countries():
     argv = ['countries']
     main(argv)
+
+def test_collective():
+    argv = ['collective']
+    main(argv)
+
+def test_individual():
+    argv = ['individual']
+    main(argv)
+
